11cfba20eb0f244792c163f35db71829ac0336c7
moloch-- <fierynudist@gmail.com>
2012-08-22 14:27:17 -0700

    Bug fixes to recovery console

--- setup/recovery.py
+++ setup/recovery.py
@@ -31,11 +31,17 @@ from models import dbsession, User, Permission
 class RecoveryConsole(cmd.Cmd):
     ''' Recovery console for user/passwords '''
 
-    intro  = "\n *** Planetary Assault System Recovery Console ***\n"
+    intro  = "\n ===========================================\n" + \
+             "  Planetary Assault System Recovery Console \n" + \
+             " ===========================================\n\n" + \
+             "Type 'help' for a list of available commands"
     prompt = underline + "Recovery" + W + " > "
 
     def do_reset(self, username):
-        ''' Reset a users password '''
+        ''' 
+        Reset a user's password
+        Usage: reset <user name>
+        '''
         user = User.by_user_name(username)
         if user == None:
             print WARN + str("%s user not found in database." % username)
@@ -43,19 +49,72 @@ class RecoveryConsole(cmd.Cmd):
             print INFO + str("Loaded %s from database" % user.user_name)
             sys.stdout.write(PROMPT + "New ")
             sys.stdout.flush()
-            new_password = getpass.getpass()
-            user.password = new_password
+            user.password = getpass.getpass()
             dbsession.add(user)
             dbsession.flush()
             print INFO + str("Updated %s password successfully." % user.user_name)
 
-    def do_ls(self, *args, **kwargs):
-        ''' List all users in the database '''
+    def do_ls(self, partial):
+        ''' 
+        List all users in the database 
+        Usage: ls
+        '''
         users = User.all()
         for user in users:
-            print INFO + str("%d. %s" % (user.id, user.user_name))
+            print INFO + user.user_name
+
+    def do_delete(self, username):
+        '''
+        Delete a user from the database
+        Usage: delete <user name>
+        '''
+        user = User.by_user_name(username)
+        if user == None:
+            print WARN + str("%s user not found in database." % username)
+        else:
+            username = user.user_name
+            print WARN + str("Are you sure you want to delete %s?" % username)
+            if raw_input(PROMPT + "Delete [y/n]: ").lower() == 'y':
+                dbsession.delete(user)
+                dbsession.flush()
+                print INFO + str("Successfully deleted %s from database." % username)
+
+    def do_create(self, username):
+        '''
+        Create a new user account
+        Usage: create <user name> 
+        '''
+        user = User(
+            user_name=unicode(username),
+        )
+        dbsession.add(user)
+        dbsession.flush()
+        sys.stdout.write(PROMPT + "New ")
+        sys.stdout.flush()
+        user.password = getpass.getpass()
+        user.approved = True
+        dbsession.add(user)
+        dbsession.flush()
+        print INFO + "Successfully created, and approved new account."
+
+    def do_approve(self, username):
+        '''
+        Approve a user account
+        Usage: approve <user name>
+        '''
+        user = User.by_user_name(username)
+        if user == None:
+            print WARN + str("%s user not found in database." % username)
+        else:
+            user.approved = True
+            dbsession.add(user)
+            dbsession.flush()
+            print INFO + str("Successfully approved %s's account." % user.user_name)
 
     def do_exit(self, *args, **kwargs):
-        ''' Exit recovery console '''
+        ''' 
+        Exit recovery console
+        Usage: exit
+        '''
         print INFO + "Have a nice day!"
         os._exit(0)
